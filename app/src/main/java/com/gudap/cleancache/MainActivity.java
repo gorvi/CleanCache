cb0d308f473d455e249b6f2a00219e951401a1f4
gaohongwei <gorvi@163.com>
2015-02-11 01:50:41 +0800

    MainActivity edit

--- app/src/main/java/com/gudap/cleancache/MainActivity.java
+++ app/src/main/java/com/gudap/cleancache/MainActivity.java
@@ -1,16 +1,23 @@
 package com.gudap.cleancache;
 
+import android.widget.RelativeLayout;
+
 /**
  * Created by ghw on 2015/2/10.
  */
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity {
+
+    RelativeLayout relativeLayout_progressBar;
     @Override
     public void setContentView() {
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     public void initViews() {
+        relativeLayout_progressBar = (RelativeLayout) findViewById(R.id.relativeLayout_progressBar);
+
 
     }
 
